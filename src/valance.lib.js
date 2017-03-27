8bfc4ddeeb42331df1a04aedc13fc75d5fbd53b2
Dan Stephenson <ispyhumanfly@gmail.com>
2017-03-27 09:12:14 -0400

    A little closer to simpifying the transpiling process at build time.

--- src/valance.lib.js
+++ src/valance.lib.js
@@ -1,14 +1,8 @@
 "use strict";
-
-exports.__esModule = true;
-
-var tsc = require("typescript-compiler");
-
-var Valance = function() {
-    function Valance(options) {
-        tsc.compile([ "./src/valance.ts" ], [ "--out", "./dist/main.js" ]);
+const tsc = require("typescript-compiler");
+class Valance {
+    constructor(options) {
+        tsc.compile(["./src/valance.ts"], ["--out", "./dist/main.js"]);
     }
-    return Valance;
-}();
-
+}
 exports.Valance = Valance;
--- src/valance.spec.js
+++ src/valance.spec.js
@@ -1,12 +1,10 @@
 "use strict";
-
-exports.__esModule = true;
-
-describe("Valance Distribution Files", function() {
-    var valance;
-    beforeEach(function() {
+describe("Valance Distribution Files", () => {
+    let valance;
+    beforeEach(() => {
         valance.cli("./dist/valance.cli.js");
         valance.lib("./dist/valance.lib.js");
     });
-    it("Checks to see if all distribution files have been built.", function() {});
+    it("Checks to see if all distribution files have been built.", () => {
+    });
 });
